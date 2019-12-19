49026ee7ebaeebff523cb2149779519aeae6b517
Shubham Arora <shubhama1903@gmail.com>
2019-12-19 16:36:20 -0500

    added spark build files

---
+++ build/digital-equity/precache-manifest.6f35a766afbb54eece81b906fd1cbd12.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "270717ecadb49de1a2d7140e91e74ec4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fadf87a7c3ff6f227cd3",
+    "url": "./static/css/main.0d128b69.chunk.css"
+  },
+  {
+    "revision": "96960de3ffc8d3e5dd24",
+    "url": "./static/js/2.7e5f7387.chunk.js"
+  },
+  {
+    "revision": "fadf87a7c3ff6f227cd3",
+    "url": "./static/js/main.3a81a54f.chunk.js"
+  },
+  {
+    "revision": "a7afe0ce0f2916aa4e47",
+    "url": "./static/js/runtime-main.cdb524c6.js"
+  }
+]);
