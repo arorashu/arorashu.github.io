e8dad7d3ca0042419548303c27972c5b67704ae7
Shubham Arora <aroras@bu.edu>
2020-05-28 10:38:01 -0400

    added p2p share build

---
+++ build/p2p-share/precache-manifest.6cc9f8498256f4237d5265169819a98b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c26659db39d1258c38fc5f20540bbc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49af74260ee061c0f318",
+    "url": "/static/css/main.94567b7f.chunk.css"
+  },
+  {
+    "revision": "72b379199b80f3bff8e2",
+    "url": "/static/js/2.b4e29fb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b4e29fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49af74260ee061c0f318",
+    "url": "/static/js/main.e75a5294.chunk.js"
+  },
+  {
+    "revision": "7959c4ab08fdc0212820",
+    "url": "/static/js/runtime-main.6d10d9e7.js"
+  }
+]);
